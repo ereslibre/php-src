63834095ff9d25d14c092b933eea532693958ef3
Rafael Fernández López <ereslibre@ereslibre.es>
2023-06-02 13:14:16 +0200

    WIP: mail.c

--- ext/standard/wasi/mail.c
+++ ext/standard/wasi/mail.c
@@ -16,17 +16,11 @@
 
 #include "php.h"
 
-/* {{{ PHP_MINFO_FUNCTION */
-PHP_MINFO_FUNCTION(mail)
-{
-	char *sendmail_path = INI_STR("sendmail_path");
-	php_info_print_table_row(2, "Path to sendmail", sendmail_path);
+PHP_MINFO_FUNCTION(mail) {
+	DISPLAY_INI_ENTRIES();
 }
-/* }}} */
 
-/* {{{ php_mail */
 PHPAPI int php_mail(const char *to, const char *subject, const char *message, const char *headers, const char *extra_cmd)
 {
 	return 0;
 }
-/* }}} */
